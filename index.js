b7a61df764923a41c346116f8f723f4b94e37100
Jeevesh0207 <r27abbit@gmail.com>
2024-01-29 08:29:29 +0530

    Crash Fix

--- index.js
+++ index.js
@@ -32,78 +32,110 @@ app.get('/checksetup', async (req, res) => {
     res.send("Ready")
 })
 
-app.post('/', async (req, res) => {
-    const { ContentLength, URL, Duration } = req.body;
-    res.setHeader('Content-Type', 'video/mp4');
-    let videoduration = 0;
-
-    const isValid = ytdl.validateURL(URL)
-    if (!isValid) {
-        res.status(500).send("ERROR")
-        res.end()
-    } else {
-        const Video = ytdl(URL, {
-            filter: (format) => {
-                if (format.contentLength === ContentLength) {
-                    videoduration = Math.max(videoduration, format.contentLength / 1024 / 1024);
-                }
-                return format.contentLength === ContentLength;
-            },
-        });
-
-        const Audio = ytdl(URL, {
-            filter: (format) => {
-                return format.audioQuality === "AUDIO_QUALITY_MEDIUM";
-            },
-        });
 
 
-        const ffmpegProcess = cp.spawn(
-            ffmpegStatic,
-            [
-                '-loglevel', '8',
-                '-hide_banner',
-                '-i', 'pipe:3',
-                '-i', 'pipe:4',
-                '-t', Duration,
-                '-map', '0:a',
-                '-map', '1:v',
-                '-c:v', 'copy',
-                '-c:a', 'copy',
-                '-preset', 'ultrafast',
-                '-f', 'matroska',
-                // '-f', 'mp4',
-                'pipe:5',
-            ],
-            {
-                windowsHide: true,
-                stdio: ["inherit", "inherit", "inherit", "pipe", "pipe", "pipe"],
-            }
-        );
+io.on("connection", (socket) => {
+    console.log(`Connected to ${socket.id}`)
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 
-        Video.pipe(ffmpegProcess.stdio[4]);
-        Audio.pipe(ffmpegProcess.stdio[3]);
+    setInterval(() => {
+        console.log('This is a message from the server')
+        socket.emit('message', 'This is a message from the server');
+    }, 600000); 
 
-        ffmpegProcess.stdio[5].pipe(res)
-        
-        let currentDuration=0
+    // -------------------------
 
-        ffmpegProcess.stdio[5].on("data", (data) => {
-            currentDuration += data.length
-            if (data) {
-                io.emit('data sent', {
-                    size: Math.floor((currentDuration / (1024 * 1024))), duration: Math.floor(videoduration)
+    app.post('/', async (req, res) => {
+        try {
+            const { ContentLength, URL, Duration, User_ID } = req.body;
+            res.setHeader('Content-Type', 'video/mp4');
+            let videoduration = 0;
+
+            const isValid = ytdl.validateURL(URL)
+            if (!isValid) {
+                res.status(500).send("ERROR")
+                res.end()
+            } else {
+                const Video = ytdl(URL, {
+                    filter: (format) => {
+                        if (format.contentLength === ContentLength) {
+                            videoduration = Math.max(videoduration, format.contentLength / 1024 / 1024);
+                        }
+                        return format.contentLength === ContentLength;
+                    },
                 });
-            }
-        })
-        ffmpegProcess.stdio[5].on("download start", () => {
-            io.emit("end")
-        })
-    }
-})
 
+                const Audio = ytdl(URL, {
+                    filter: (format) => {
+                        return format.audioQuality === "AUDIO_QUALITY_MEDIUM";
+                    },
+                });
+
+
+                const ffmpegProcess = cp.spawn(
+                    ffmpegStatic,
+                    [
+                        '-loglevel', '8',
+                        '-hide_banner',
+                        '-i', 'pipe:3',
+                        '-i', 'pipe:4',
+                        '-t', Duration,
+                        '-map', '0:a',
+                        '-map', '1:v',
+                        '-c:v', 'copy',
+                        '-c:a', 'copy',
+                        '-preset', 'ultrafast',
+                        '-f', 'matroska',
+                        'pipe:5',
+                    ],
+                    {
+                        windowsHide: true,
+                        stdio: ["inherit", "inherit", "inherit", "pipe", "pipe", "pipe"],
+                    }
+                );
+
+                Video.pipe(ffmpegProcess.stdio[4]);
+                Audio.pipe(ffmpegProcess.stdio[3]);
+
+                ffmpegProcess.stdio[5].pipe(res)
+
+                let currentDuration = 0
+
+                ffmpegProcess.stdio[5].on("data", (data) => {
+                    currentDuration += data.length
+                    if (data) {
+                        socket.to(User_ID).emit('data sent', {
+                            size: Math.floor((currentDuration / (1024 * 1024))), duration: Math.floor(videoduration)
+                        });
+                    }
+                })
+                ffmpegProcess.stdio[5].on("download start", () => {
+                    io.emit("end")
+                })
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("An error occurred");
+        }
+    })
+
+    // --------------------------------
+
+})
 const port = 4000
 
 server.listen(port, () => {
     console.log(`Backend listening on port ${port}`);
+});
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("Node NOT Exiting...");
+    // res.send("ERROR")
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason);
 });
